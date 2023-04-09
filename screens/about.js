3a55e66f1b21c98e63a1416862befaa126344315
lecbit <artm-marych@rambler.ru>
2023-04-09 07:37:08 +0100

    ~18~ Global Styles

--- screens/about.js
+++ screens/about.js
@@ -1,17 +1,11 @@
-import { StyleSheet } from "react-native";
+import { globalStyles } from "../styles/global";
 import { Text } from "react-native";
 import { View } from "react-native";
 
 export default function About() {
     return (
-        <View style={styles.container}>
-            <Text>About Screen!</Text>
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}>About Screen!</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container:{
-        padding: 24,
-    }
-});
--- screens/reviewDetail.js
+++ screens/reviewDetail.js
@@ -1,17 +1,11 @@
-import { StyleSheet } from "react-native";
+import { globalStyles } from "../styles/global";
 import { Text } from "react-native";
 import { View } from "react-native";
 
 export default function ReviewDetails() {
     return (
-        <View style={styles.container}>
-            <Text>ReviewDetails Screen!</Text>
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}>ReviewDetails Screen!</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container:{
-        padding: 24,
-    }
-});
